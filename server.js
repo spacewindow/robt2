d3623f1bee67214a82ebe929a8dcda4accd5e5b5
Robert Thwaites <robertthwaites@JOBSs-MacBook-Pro.local>
2019-02-19 09:50:04 +1100

    DJSB Analytics Pages

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 const tinycolor = require("tinycolor2");
 
-const {data} = require("./js/data.js");
+const { data } = require("./js/data.js");
 
-app.set('port', (process.env.PORT || 8080));
+app.set("port", process.env.PORT || 8080);
 
 app.use(express.static(__dirname));
 
 const color = {
-  black: '#222222',
-  white: '#ffffff',
-  palegrey: '#fafafa',
-  lightgrey: '#efefef',
-  midgrey:  '#aaaaaa',
-  darkgrey: '#333333',
-  orange: '#ff9900'
+  black: "#222222",
+  white: "#ffffff",
+  palegrey: "#fafafa",
+  lightgrey: "#efefef",
+  midgrey: "#aaaaaa",
+  darkgrey: "#333333",
+  orange: "#ff9900"
 };
 
 // // get list of projects
@@ -27,28 +27,32 @@ const color = {
 // });
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // default pageData
 
-app.get('/', function(req, res) {
-  res.render('../views/index.html.ejs', {
+app.get("/", function(req, res) {
+  res.render("../views/index.html.ejs", {
     allData: data,
     color: color,
     tinycolor: tinycolor
   });
 });
 
-app.get('/about', function(req, res) {
-  res.render('../views/about.html.ejs', {
+app.get("/djsb", function(req, res) {
+  res.render("../views/djsb/index.html.ejs");
+});
+
+app.get("/about", function(req, res) {
+  res.render("../views/about.html.ejs", {
     allData: data,
     color: color,
     tinycolor: tinycolor
   });
 });
 
-app.get('/video', function(req, res) {
-  res.render('../views/video.html.ejs', {
+app.get("/video", function(req, res) {
+  res.render("../views/video.html.ejs", {
     allData: data,
     color: color,
     tinycolor: tinycolor
@@ -57,22 +61,22 @@ app.get('/video', function(req, res) {
 
 // any page that is not the root / homepage
 
-app.get('/*', function(req, res) {
+app.get("/*", function(req, res) {
   // if request is homepage
   let pageData = {};
-  let reqPage = req.url.replace('/', '');
-  console.log('reqpage', reqPage);
+  let reqPage = req.url.replace("/", "");
+  console.log("reqpage", reqPage);
   // check if request is a page in the data
   let dataIndex = data.findIndex(p => p.id === reqPage);
-  if(typeof dataIndex !== 'undefined'){
+  if (typeof dataIndex !== "undefined") {
     // if the page exists, set up the data
     pageData.current = data.filter(p => p.id === reqPage)[0];
     pageData.previous = dataIndex > 0 ? data[dataIndex - 1] : null;
-    pageData.next = dataIndex < (data.length - 1) ? data[dataIndex + 1] : null;
+    pageData.next = dataIndex < data.length - 1 ? data[dataIndex + 1] : null;
   }
 
   // render page. An unknown page will trigger express error
-  res.render('../views/projects/' + reqPage + '.html.ejs', {
+  res.render("../views/projects/" + reqPage + ".html.ejs", {
     pageData: pageData,
     allData: data,
     color: color,
@@ -90,6 +94,6 @@ app.get('/*', function(req, res) {
 //   res.render('../views/404.html.ejs');
 // });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function() {
+  console.log("Node app is running on port", app.get("port"));
 });
